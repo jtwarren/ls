d152b8e0c74e5158660e24841e1bce3e5defc927
Jeffrey Warren <jtwarren@mit.edu>
2013-12-28 19:21:04 -0500

    removed main from server

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ from learn_sprout import LinguisticChains
 
 app = Flask(__name__)
 
+words = set(line.strip() for line in open('words'))
+
+lc = LinguisticChains(words)
+
 # Not sure how to do this, want lc to persist while server runs.
 # Effect is that it caches results (longest_chains is memoized).
 lc = LinguisticChains([])
@@ -29,12 +33,12 @@ def linguistic_chains():
   # Return json of chains
   return jsonify({starting_word: chains})
 
-if __name__ == '__main__':
-  # For server, use words dictionary
-  words = set(line.strip() for line in open('words'))
+# if __name__ == '__main__':
+#   # For server, use words dictionary
+#   words = set(line.strip() for line in open('words'))
 
-  # Set LinguisticChains to use words
-  lc = LinguisticChains(words)
+#   # Set LinguisticChains to use words
+#   lc = LinguisticChains(words)
 
-  # Run server
-  app.run(debug = True)
+#   # Run server
+#   app.run(debug = True)
